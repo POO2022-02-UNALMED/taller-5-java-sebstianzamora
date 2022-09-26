7887634a573a1a799ef7f7abda8c4a8a225631e5
sebstianzamora <69611081+sebstianzamora@users.noreply.github.com>
2022-09-26 10:32:40 -0500

    .

---
+++ src/main/java/zooAnimales/Reptil.java
@@ -0,0 +1,64 @@
+package zooAnimales;
+
+import java.util.ArrayList;
+
+public class Reptil extends Animal {
+	
+	private static ArrayList<Reptil> listado = new ArrayList<Reptil>();
+	public static int iguanas;
+	public static int serpientes;
+	private String colorEscamas;
+	private int largoCola;
+	
+	public Reptil() {
+		this(null,0,null,null,null,0);
+	}
+	
+	public Reptil(String nombre, int edad, String habitat, String genero,String colorEscamas, int largoCola) {
+		super(nombre,edad,habitat,genero);
+		this.colorEscamas=colorEscamas;
+		this.largoCola=largoCola;
+		listado.add(this);
+	}
+
+	public static ArrayList<Reptil> getListado() {
+		return listad;
+	}
+
+	public static void setListado(ArrayList<Reptil> listado) {
+		Reptil.listado = listado;
+	}
+
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas = colorEscamas;
+	}
+
+	public int getLargoCola() {
+		return largoCola;
+	}
+
+	public void setLargoCola(int largoCola) {
+		this.largoCola = largoCola;
+	}
+	
+	public static Animal crearSerpiente(String nombre, int edad, String genero) {
+		serpientes++;
+		return new Reptil(nombre,edad,"jungla",genero,"blanco",1);
+	}
+	public static Animal crearIguana(String nombre, int edad, String genero) {
+		iguanas++;
+		return new Reptil(nombre,edad,"humedal",genero,"verde",3);
+	}
+	public static int cantidadReptiles() {
+		return listado.size();
+	}
+	
+	@Override
+	public String movimiento() {
+		return "reptar";
+	}
+}
